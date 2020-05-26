4719c97317189e5997f71fa2a54ddddd1069a520
segwuonwu <segwuonwu@gmail.com>
2020-05-26 16:26:14 -0700

    fix the  bug on the nav

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-$(document).ready(function () {
+$(document).ready(function(){
     var centerShelfs,
         $body = $('body'),
         $topShelf = $('.shelf.top'),
         $middleShelf = $('.shelf.middle'),
         $bottomShelf = $('.shelf.bottom');
-    
-    centerShelfs = function () {
-        var topShelfPosition = $body.height() / 2;
-
-        $topShelf.css('top', topShelfPosition);
-        $middleShelf.css('top', topShelfPosition + 200)
-        $bottomShelf.css('top', topShelfPosition + 400)
+  
+    centerShelfs = function(){
+      var topShelfPosition = $body.height()/2;
+  
+      $topShelf.css('top', topShelfPosition);
+      $middleShelf.css('top', topShelfPosition + 200);
+      $bottomShelf.css('top', topShelfPosition + 400);
     };
 
-    moveToShelf = function (e) {
-        e.preventDefault();
-        $body.attr('class', '');
-        $body.addClass(e.target.id);
+    moveToShelf = function(e){
+      e.preventDefault();
+      $body.attr('class', '');
+      $body.addClass(e.target.id);
     };
-
-    //bind events
+  
+    // bind events
     $(window).on('resize', centerShelfs);
     $('.nav a').on('click', moveToShelf);
-
+  
     // move to start position
     centerShelfs();
-
-    window.setTimeout(function () {
-        $body.addClass('view-middle-shelf');
+    
+    window.setTimeout(function(){
+      $body.addClass('view-middle-shelf');
     }, 500);
-});
+  });
+  
